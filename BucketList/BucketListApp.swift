cf424f0df38fa7dfc2cfb68c84a2a9c75b6e2330
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-07-23 12:29:39 +0900

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Hiroshi Inoue on R 5/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
